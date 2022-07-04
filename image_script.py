40b7135d925ff645b5e5d91985252dcd60f89167
Roderick Davis <rdavis3483@gmail.com>
2022-07-03 20:50:40 -0400

    Skeleton of program created

--- image_script.py
+++ image_script.py
@@ -3,5 +3,17 @@
 from PIL import Image
 import os, sys
 
+file = 'rotateimage.jpg'
 
+im = Image.open(file).convert('RGB')
+
+print('Image specs:\nFormat - {}\nSize - {}'.format(im.format, im.size))
+
+
+x = int(input('New size (x): '))
+y = int(input('New size (y): '))
+newsize = (x, y)
+degrees = int(input('Degrees to rotate (90, 180, 270): '))
+im = im.resize(newsize).rotate(degrees)
+im.show()
 
